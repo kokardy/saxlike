8fa1bc9a7bd3fac2acb91809255ed9d43e767c7f
magician1 <kouta-k@mbm.nifty.com>
2013-08-26 16:30:43 +0900

    modified:   handler.go 	modified:   parser.go

--- handler.go
+++ handler.go
@@ -1,17 +1,17 @@
 package saxlike
 
 import (
-  "encoding/xml"
+	"encoding/xml"
 )
 
 //SAX-like handler
-type Handler interface{
-  StartDocument()
-  EndDocument()
-  StartElement(xml.StartElement)
-  EndElement(xml.EndElement)
-  CharData(xml.CharData)
-  Comment(xml.Comment)
-  ProcInst(xml.ProcInst)
-  Directive(xml.Directive)
+type Handler interface {
+	StartDocument()
+	EndDocument()
+	StartElement(xml.StartElement)
+	EndElement(xml.EndElement)
+	CharData(xml.CharData)
+	Comment(xml.Comment)
+	ProcInst(xml.ProcInst)
+	Directive(xml.Directive)
 }
--- parser.go
+++ parser.go
@@ -1,68 +1,77 @@
 package saxlike
+
 import (
-  "encoding/xml"
-  "io"
+	"encoding/xml"
+	"io"
+	"log"
 )
 
-type Parser Struct{
-  *xml.Decoder
-  handler *Handler
+type Parser struct {
+	*xml.Decoder
+	handler Handler
 }
 
-func NewParser(reader io.Reader, handler Handler){
-  decoder := xml.NewDecoder(reader)
-  return &Parser{decoder, handler}
+func NewParser(reader io.Reader, handler Handler) *Parser {
+	decoder := xml.NewDecoder(reader)
+	return &Parser{decoder, handler}
 }
 
-func (p *Parser)SetHTMLMode(){
-  p.Strict = false
+func (p *Parser) SetHTMLMode() {
+	p.Strict = false
 	p.AutoClose = xml.HTMLAutoClose
 	p.Entity = xml.HTMLEntity
 }
 
-func (p *Parser)Parse() (err error){
-  p.handler.StartDocument()
-  
-  for {
-    token, err := p.Token()
-    if err != io.EOF{
-      err = nil
-      break
-    }
-    if err != nil{
-      return
-    }
-    switch token.(type){
-      case xml.StartElement:
-        s := token.(xml.StartElement)
-        p.handler.StartElement(s)
-      case xml.EndElement:
-        e := token.(xml.EndElement)
-        p.handler.EndElement(e)
-      case xml.CharData:
-        c := token.(xml.CharData)
-        p.handler.CharData(c)
-      case xml.Comment(xml.Comment)
-        com := token.(xml.Comment)
-        p.handler.Comment(com)
-      case xml.ProcInst:
-        pro := token.(xml.ProcInst)
-        p.handler.ProcInst(pro)
-      case xml.Directive:
-        dir := token.(xml.Directive)
-        p.handler.Directive(dir)
-      default:
-        panic("unknown xml token.")
-    }
-  }
-  
-  p.handler.EndDocument()
+func (p *Parser) Parse() (err error) {
+	p.handler.StartDocument()
+
+	for {
+		token, err := p.Token()
+		if err == io.EOF {
+			err = nil
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		switch token.(type) {
+		case xml.StartElement:
+			log.Println("start")
+			s := token.(xml.StartElement)
+			p.handler.StartElement(s)
+		case xml.EndElement:
+			log.Println("end")
+			e := token.(xml.EndElement)
+			p.handler.EndElement(e)
+		case xml.CharData:
+			log.Println("char")
+			c := token.(xml.CharData)
+			p.handler.CharData(c)
+		case xml.Comment:
+			log.Println("comment")
+			com := token.(xml.Comment)
+			p.handler.Comment(com)
+		case xml.ProcInst:
+			log.Println("proc")
+			pro := token.(xml.ProcInst)
+			p.handler.ProcInst(pro)
+		case xml.Directive:
+			log.Println("directive")
+			dir := token.(xml.Directive)
+			p.handler.Directive(dir)
+		default:
+			panic("unknown xml token.")
+		}
+	}
+
+	p.handler.EndDocument()
+	return
 }
 
-func Parse(decoder *xml.Decoder, handler Handler, htmlMode boolean) error{
-  parser := &Parser{decoder, handler)
-  if htmlMode{
-  	parser.SetHTMLMode()
-  }
-  return parser.Parse()
+func Parse(decoder *xml.Decoder, handler Handler, htmlMode bool) error {
+	parser := &Parser{decoder, handler}
+	if htmlMode {
+		parser.SetHTMLMode()
+	}
+	return parser.Parse()
 }
