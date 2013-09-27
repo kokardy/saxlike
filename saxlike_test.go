5b373bcf00e99d78fd7e8488f6c32b80ffda0c25
magician1 <kouta-k@mbm.nifty.com>
2013-09-27 22:44:36 +0900

    test update

--- saxlike_test.go
+++ saxlike_test.go
@@ -3,20 +3,67 @@ package saxlike
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
+	"html/template"
 	"log"
+	"strings"
 	"testing"
 )
 
-type PartialHandler struct {
-	VoidHandler
+func indent(i int) string {
+	buf := bytes.NewBufferString("")
+	for n := 0; n < i; n++ {
+		buf.WriteString("\t")
+	}
+	return buf.String()
 }
 
-func (h PartialHandler) StartElement(element xml.StartElement) {
-	log.Println("start:", element.Name.Local)
+type PartialHandler struct {
+	VoidHandler
+	indent int
+}
+
+func (h *PartialHandler) StartElement(e xml.StartElement) {
+	fmt.Printf("%s<%s>\n", indent(h.indent), e.Name.Local)
+	h.indent++
+}
+
+func (h *PartialHandler) EndElement(e xml.EndElement) {
+	h.indent--
+	fmt.Printf("%s</%s>\n", indent(h.indent), e.Name.Local)
+}
+
+func (h *PartialHandler) CharData(c xml.CharData) {
+	i := indent(h.indent)
+	comment := template.HTMLEscapeString(strings.Trim(string(c), " \t\r\n"))
+	if len(comment) > 0 {
+		fmt.Printf("%s%s\n", i, comment)
+	}
+}
+
+func (h *PartialHandler) Comment(c xml.Comment) {
+	fmt.Printf("%s<!--%s-->\n", indent(h.indent), string(c))
+}
+
+func (h *PartialHandler) ProcInst(p xml.ProcInst) {
+	fmt.Printf("%s<?%s %s?>\n", indent(h.indent), p.Target, p.Inst)
 }
 
 func TestPartialHandler(t *testing.T) {
-	source := `<html> <title>taitoru</title> <body>&lt;bodfdaDF</body> </html>`
+	log.Println("handler test start")
+	source := `<html> 
+  <title>taitoru</title> 
+  <body>&lt;bodfdaDF</body>
+  <div>
+  <!-- comment  -->
+  <?target inst ?> 
+  </div>
+  <![CDATA[
+  <fdsajk><>?fdskfdsafdsajkfldsafjkl
+    fjdklasfjdkslaf
+  ]]>
+  </html>`
+	fmt.Println(source)
 	r := bytes.NewReader([]byte(source))
 	handler := &PartialHandler{}
 	parser := NewParser(r, handler)
